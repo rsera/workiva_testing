9ca92daf590d7268cfbd726182760d8b08903357
rsera <rachaelserah@gmail.com>
2025-07-31 22:21:51 -0700

    Update workiva-generate-sdk.sh

--- workiva-generate-sdk.sh
+++ workiva-generate-sdk.sh
@@ -1,11 +1,7 @@
 openapi-generator-cli generate \
-  # -i = input file 
   -i oas.yaml \
-  # -g = generator name 
   -g python \
-  # -o = output directory for the SDK
-  -o ./workiva-python-sdk 
-  # change package name
+  -o ./workiva-python-sdk
   --additional-properties packageName=workiva_sdk
 
 
